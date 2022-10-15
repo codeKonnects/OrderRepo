3a731360ec03241ad56a754b9f2e0a3fef05bef6
codeKonnects <topedaves@gmail.co>
2022-10-15 01:58:51 +0100

    set up order repo

---
+++ src/main/java/com/codeKonnects/order/MelitaApplication.java
@@ -0,0 +1,13 @@
+package com.codeKonnects.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelitaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeKonnects/order/MelitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeKonnects.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
